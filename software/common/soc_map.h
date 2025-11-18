105b1574bb93d59ff7426c1177dd180ebc8c41cf
Ahsan Riaz <ahsanriaz8000@gmail.com>
2025-11-18 11:04:03 +0500

    Enhance security by adding Key Store register definitions in soc_map.h, marking them as protected by MPU to prevent unauthorized access from user mode.

--- software/common/soc_map.h
+++ software/common/soc_map.h
@@ -18,13 +18,24 @@
 // Peripherals
 #define UART_BASE           0x20000000
 #define CRYPTO_BASE         0x30000000    // Future
-#define KEY_STORE_BASE      0x40000000    // Future
+#define KEY_STORE_BASE      0x40000000    // Protected by MPU!
 
 // UART Registers
 #define UART_TX_REG         (*(volatile unsigned int*)(UART_BASE + 0x00))
 #define UART_STATUS_REG     (*(volatile unsigned int*)(UART_BASE + 0x04))
 
 #define UART_TX_BUSY        0x01
+
+// Key Store Registers (PROTECTED - Machine mode only!)
+// Attempting to access these from user mode will cause MPU violation
+#define KEY_STORE_SIZE      0x00000100    // 256 bytes
+#define AES_KEY_0           (*(volatile unsigned int*)(KEY_STORE_BASE + 0x00))
+#define AES_KEY_1           (*(volatile unsigned int*)(KEY_STORE_BASE + 0x04))
+#define AES_KEY_2           (*(volatile unsigned int*)(KEY_STORE_BASE + 0x08))
+#define AES_KEY_3           (*(volatile unsigned int*)(KEY_STORE_BASE + 0x0C))
+#define HMAC_KEY_0          (*(volatile unsigned int*)(KEY_STORE_BASE + 0x10))
+#define HMAC_KEY_1          (*(volatile unsigned int*)(KEY_STORE_BASE + 0x14))
+#define ROOT_KEY            (*(volatile unsigned int*)(KEY_STORE_BASE + 0x20))
 
 #endif // SOC_MAP_H
 
